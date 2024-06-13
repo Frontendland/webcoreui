c7103115bb69370ee04e35e4f7b20ecd3e71bab6
Frontendland <ferenc@frontendland.com>
2024-06-13 16:38:08 +0200

    ✨ Make fonts configurable, add type declaration and reexports

---
+++ src/astro.d.ts
@@ -0,0 +1,5 @@
+declare module 'webcoreui/astro' {
+    import type { Props as AccordionProps } from './components/Accordion/Accordion.astro'
+
+    export function Accordion(_props: AccordionProps): any
+}
---
+++ src/astro.js
@@ -0,0 +1,3 @@
+import Accordion from './components/Accordion/Accordion.astro'
+
+export const Accordion = Accordion
