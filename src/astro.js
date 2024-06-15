a72ff5e6e2bb4623b49f4e3593a679c062a24481
Frontendland <ferenc@frontendland.com>
2024-06-15 14:15:40 +0200

    ✨ Add button and card exports to astro/svelte/react

--- src/astro.js
+++ src/astro.js
@@ -1,3 +1,7 @@
 import AccordionComponent from './components/Accordion/Accordion.astro'
+import ButtonComponent from './components/Button/Button.astro'
+import CardComponent from './components/Card/Card.astro'
 
 export const Accordion = AccordionComponent
+export const Button = ButtonComponent
+export const Card = CardComponent
--- src/svelte.js
+++ src/svelte.js
@@ -1,3 +1,7 @@
-import { Accordion as AccordionComponent } from './components/Accordion/Accordion.svelte'
+import AccordionComponent from './components/Accordion/Accordion.svelte'
+import ButtonComponent from './components/Button/Button.svelte'
+import CardComponent from './components/Card/Card.svelte'
 
 export const Accordion = AccordionComponent
+export const Button = ButtonComponent
+export const Card = CardComponent
