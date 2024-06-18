f34899dae5a91bdf154509274be06980dc93b130
Frontendland <ferenc@frontendland.com>
2024-06-18 13:45:06 +0200

    ✨ Add Avatar component for Astro

---
+++ src/components/Avatar/avatar.ts
@@ -0,0 +1,10 @@
+export type AvatarProps = {
+    img: string | string[]
+    alt?: string | string[]
+    size?: number | number[]
+    lazy?: boolean
+    borderColor?: string
+    borderless?: boolean
+    reverse?: boolean
+    className?: string
+}
