a56fb4ef92a4eca0366670ec05fe0e997a37533a
Frontendland <ferenc@frontendland.com>
2024-06-19 17:31:04 +0200

    ✨ Add Switch component for Astro

---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import type { SwitchProps } from './switch'
+import './switch.scss'
+
+const Switch = ({
+    
+}: SwitchProps) => {
+
+
+    return (
+        <div>switch</div>
+    )
+}
+
+export default Switch
---
+++ src/components/Switch/switch.ts
@@ -0,0 +1,11 @@
+export type SwitchProps = {
+    label?: string
+    toggled?: boolean
+    offColor?: string
+    onColor?: string
+    reverse?: boolean
+    small?: boolean
+    square?: boolean
+    disabled?: boolean
+    className?: string
+}
