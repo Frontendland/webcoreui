6f2a834a8a51e1ca42dfee4e0572d90e25161688
Frontendland <ferenc@frontendland.com>
2024-06-20 22:46:22 +0200

    🔨 Improve build types script

--- scripts/buildTypes.js
+++ scripts/buildTypes.js
@@ -1,19 +1,52 @@
 import fs from 'fs'
 
+const format = template => template.trim().replace(new RegExp('^[ \\t]{12}', 'gm'), '')
+
 const buildTypes = type => {
     const components = fs.readdirSync('src/components')
 
-    return `
-declare module 'webcoreui/${type}' {
-    ${components.map(component => {
-        return `import type { ${component}Props } from './components/${component}/${component.toLowerCase()}'`
-    }).join('\n\t')}
+    if (type === 'astro') {
+        return format(`
+            ${components.map(component => {
+                return `import type { ${component}Props } from './components/${component}/${component.toLowerCase()}'`
+            }).join('\n')}
+    
+            declare module 'webcoreui/${type}' {
+                ${components.map(component => {
+                    return `export function ${component}(_props: ${component}Props): any`
+                }).join('\n\t')}
+            }
+        `)
+    }
 
-    ${components.map(component => {
-        return `export function ${component}(_props: ${component}Props): any`
-    }).join('\n\t')}
-}
-    `
+    if (type === 'svelte') {
+        return format(`
+            ${components.map(component => {
+                return `import type { ${component}Props } from './components/${component}/${component.toLowerCase()}'`
+            }).join('\n')}
+    
+            declare module 'webcoreui/${type}' {
+                ${components.map(component => {
+                    return `export function ${component}(_props: ${component}Props): any`
+                }).join('\n\t')}
+            }
+        `)
+    }
+
+    if (type === 'react') {
+        return format(`
+            import { FC } from 'react'
+            ${components.map(component => {
+                return `import type { ${component}Props } from './components/${component}/${component.toLowerCase()}'`
+            }).join('\n')}
+
+            declare module 'webcoreui/${type}' {
+                ${components.map(component => {
+                    return `export const ${component}: FC<${component}Props>`
+                }).join('\n\t')}
+            }
+        `)
+    }
 }
 
 export default buildTypes
