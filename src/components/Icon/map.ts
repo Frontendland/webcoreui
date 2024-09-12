0e79854dfb7be433e4d238f6087fd011d5b7178e
Frontendland <ferenc@frontendland.com>
2024-09-12 14:56:50 +0200

    ✨ Add sorting functionality to Astro data tables

--- src/components/Icon/map.ts
+++ src/components/Icon/map.ts
@@ -8,6 +8,7 @@ import Close from '../../icons/close.svg?raw'
 import Github from '../../icons/github.svg?raw'
 import Info from '../../icons/info.svg?raw'
 import Moon from '../../icons/moon.svg?raw'
+import Order from '../../icons/order.svg?raw'
 import Search from '../../icons/search.svg?raw'
 import Sun from '../../icons/sun.svg?raw'
 import Warning from '../../icons/warning.svg?raw'
@@ -23,6 +24,7 @@ const iconMap = {
     'github': Github,
     'info': Info,
     'moon': Moon,
+    'order': Order,
     'search': Search,
     'sun': Sun,
     'warning': Warning
