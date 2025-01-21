610bfafeb8bf4f673e25ca3832d2c8259425bf0d
Frontendland <ferenc@frontendland.com>
2025-01-21 11:05:47 +0100

    🔥 Remove comments

--- src/data.ts
+++ src/data.ts
@@ -654,22 +654,3 @@ export const getPortfolioTemplateData = (theme = 'success') => ({
         ]
     }
 })
-
-// items: {
-//     icon?: IconProps['type'] | string
-//     title?: string
-//     text: string
-// }[]
-// columns?: 1 | 2 | 3 | 4
-// alignment?: 'center' | 'right'
-// iconWithBackground?: boolean
-// secondary?: boolean
-// className?: string
-
-// servicesTitle?: string
-// services?: GridWithIconsProps
-// servicesCta?: {
-//     text: string
-//     icon?: IconProps['type'] | string
-// } & ButtonProps
-// [key: string]: any
