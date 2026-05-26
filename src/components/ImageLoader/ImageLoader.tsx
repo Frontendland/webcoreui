236528edd89e5bf2a83163f530e146a6f93c091c
Frontendland <ferenc@frontendland.com>
2026-05-26 17:27:13 +0200

    🔥 Remove unused imports

--- src/components/ImageLoader/ImageLoader.tsx
+++ src/components/ImageLoader/ImageLoader.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import type { ImageLoaderProps } from './imageloader'
 
 import Image from '../Image/Image.tsx'
